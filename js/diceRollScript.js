971233d2e2d5e93d99a073ef1c5eaebaffd9a641
Flavio Bianchi <flaviopbianchi@gmail.com>
2023-03-15 18:47:52 -0300

    V1.0.2

--- js/diceRollScript.js
+++ js/diceRollScript.js
@@ -19,24 +19,58 @@ function mais() {
     atualizarQuantidadeDados();
 };
 
+
+var modificadorDados = 0;
+
+function atualizarMod() {
+    var elementModificador = document.getElementById("modificador");
+    elementModificador.innerHTML = modificadorDados;
+}
+
+var addMod = document.getElementById("menosModificador");
+function menosMod() {
+    modificadorDados--;
+    atualizarMod();
+};
+
+
+function maisMod() {
+    modificadorDados++;
+    atualizarMod();
+};
+
 function rolarDados() {
-    var numeroLados = 6;
-    var inputsRadioNumeroLados = document.getElementsByName("numeroLados");
-    for (var i = 0; i < inputsRadioNumeroLados.length; i++) {
-        if (inputsRadioNumeroLados[i].checked) {
-            numeroLados = parseInt(inputsRadioNumeroLados[i].value);
-            break;
-        }
+
+    var numeroLados = 0;
+    var inputNumeroLados = document.querySelector('input[name="numeroLados"]:checked');
+
+    if (inputNumeroLados) {
+        numeroLados = parseInt(inputNumeroLados.value);
     }
+
     var menorValor = quantidadeDados;
     var maiorValor = quantidadeDados * numeroLados;
     var resultado = Math.floor(Math.random() * (maiorValor - menorValor + 1)) + menorValor;
 
     var resultadoElemento = document.getElementById("resultado");
-    resultadoElemento.innerHTML = resultado;
+    resultadoElemento.innerHTML = resultado + modificadorDados;
 
     return resultado;
 }
+
+var inputsRadioNumeroLados = document.getElementsByName("numeroLados");
+for (var i = 0; i < inputsRadioNumeroLados.length; i++) {
+    inputsRadioNumeroLados[i].addEventListener('change', function () {
+        for (var j = 0; j < inputsRadioNumeroLados.length; j++) {
+            if (inputsRadioNumeroLados[j].checked) {
+                inputsRadioNumeroLados[j].parentNode.classList.add('selecionado');
+            } else {
+                inputsRadioNumeroLados[j].parentNode.classList.remove('selecionado');
+            }
+        }
+    });
+}
+
 
 
 
